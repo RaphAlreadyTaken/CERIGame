62b0b7d2e2a4c8858ca2ba89362492b7ce6d7989
Loic MENGUY <loicm97@gmail.com>
2018-10-08 18:57:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 /******** Chargement des Middleware
 *
 ********/
+const express = require('express'); // définit expressJS
+var http = require('http');
 
 /******** Declaration des variables
-*
-********/
+ *
+ ********/
+const app = express(); // appel à expressJS
 
 /******** Configuration du serveur NodeJS - Port : 3xxx
 *
@@ -14,3 +17,13 @@
 *
 ********/
 
+
+app.get('/', function (req, res) 
+{
+    res.send("/home/nas02a/etudiants/inf/uapv1603044/squeletteWeb1819/CERIGame/index.html");
+});
+
+var server=app.listen(3131, function()
+{
+    console.log('listening on 3131'); // Message dans la console Node
+}); 
