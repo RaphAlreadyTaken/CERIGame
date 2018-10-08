96bd81e8c0ed52c400a6a6943d1e248ff7a7b9b3
Loic MENGUY <loicm97@gmail.com>
2018-10-08 17:08:50 +0200

    Skeleton is now here :(
    push

--- README.md
+++
@@ -1,2 +0,0 @@
-# CERIGame
-The game of the CERI
---
+++ server.js
@@ -0,0 +1,16 @@
+/******** Chargement des Middleware
+*
+********/
+
+/******** Declaration des variables
+*
+********/
+
+/******** Configuration du serveur NodeJS - Port : 3xxx
+*
+********/
+
+/******** Gestion des URI
+*
+********/
+
