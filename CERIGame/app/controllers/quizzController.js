396cd2bab369ff1b9120ea7d450c14a2f69c7480
RaphaelMontet <raflegrand@hotmail.com>
2018-12-03 03:06:09 +0100

    Quizz questions / réponses 80%
    - Déroulement du quizz fait.
    
    - Affichage du bilan fait.
    
    - Calcul du score fait.
    
    TODO:
    - Rajout du score dans base historique (+ récupération de l'heure du quizz)
    - Enchaînement propre de quizzs (remise à 0 des variables)

--- CERIGame/app/controllers/quizzController.js
+++ CERIGame/app/controllers/quizzController.js
@@ -12,19 +12,24 @@ function quizzController($scope, $interval, quizz, user)
         $scope.chrn = "00:00";    //Chronomètre
         sCpt = 0;  //Compteur de secondes
 
-        var mnome = $interval(function()
-        {
+        mnome = $interval(function()
+        {   
+            if ($scope.stopChrono === true)
+            {
+                return;
+            }
+
             sCpt += 1;
-    
             $scope.chrn = (Math.floor(sCpt / 600) % 60) + "" + (Math.floor(sCpt / 60) % 10) + ":" + (Math.floor(sCpt / 10) % 6) + "" + sCpt % 10;
+
+            if (sCpt == 3600)   //Dépassement d'1h
+            {
+                $scope.chrn = "u ded?";
+                return;
+            }
         }, 1000)
         
-        if (sCpt == 3600)   //Dépassement d'1h
-        {
-            $scope.chrn = "u ded?";
-            clearInterval(mnome);
-        }
-
+        clearInterval(mnome);
     }
     
     $scope.obtainQuestions = function(nbQ, theme)
@@ -32,8 +37,8 @@ function quizzController($scope, $interval, quizz, user)
         quizz.getQuestion(nbQ, theme)
         .then(function(response)
         {
-            $scope.question = response.data;
-            
+            $scope.questions = response.data; 
+            console.log($scope.questions);
         });
     };
 
@@ -46,4 +51,72 @@ function quizzController($scope, $interval, quizz, user)
         });
     };
     $scope.obtainThemes();
+
+    $scope.showNextQuestion = function($index)
+    {
+        document.getElementById("question" + $index).style.display = "none";
+
+        if (document.getElementById("question" + ($index + 1)) !== null)
+        {
+            document.getElementById("question" + ($index + 1)).style.display = "initial";
+        }
+        else
+        {
+            $scope.endQuizz = true;
+        }
+    };
+
+    $scope.checkQuizzStatus = function(propos, $index)
+    {
+        $scope.showNextQuestion($index);
+        $scope.storeAnswer(propos);
+
+        if ($scope.endQuizz === true)
+        {
+            $scope.resultatQuizz();
+            $scope.recapQuizz = true;
+            $scope.stopChrono = true;
+        } 
+    }
+
+    $scope.answerSet = [];
+
+    $scope.storeAnswer = function(prop)
+    {
+        $scope.answerSet.push(prop);
+        console.log("%o", $scope.answerSet);
+    }
+
+    
+    $scope.resultatQuizz = function()
+    {
+        nbOkRep = 0;
+        $scope.bilan = [];
+
+        for (var i = 0; i < $scope.answerSet.length; i++)
+        {
+            value = {};
+            value['question'] = $scope.questions[i]['question'];
+            value['expRep'] = $scope.questions[i]['réponse'];
+
+            if ($scope.answerSet[i] === $scope.questions[i]['réponse'])
+            {
+                value['givRep'] = "Bonne réponse";
+                nbOkRep++;
+            }
+            else if ($scope.answerSet[i] === undefined)
+            {
+                value['givRep'] = "Aucune réponse";
+            }
+            else
+            {
+                value['givRep'] = "Mauvaise réponse";
+            }
+
+            $scope.bilan.push(value);
+        }
+
+        score = Math.floor((nbOkRep * 1398.2) / sCpt);
+        $scope.bilan['score'] = score;
+    }
 };
